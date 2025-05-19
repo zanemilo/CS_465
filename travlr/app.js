42411b0c0084c168fc034f1818365c88b2fc192b
zanedeso <zanemilo@yahoo.com>
2025-05-19 16:50:03 -0400

    Register handlebars partials for improved view rendering

--- travlr/app.js
+++ travlr/app.js
@@ -7,11 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var travelRouter = require('./routes/travel');
+var handlebars = require('hbs');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
+
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
