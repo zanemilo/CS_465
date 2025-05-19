4919bff9d20cc56282773f3801aaeb36bd6314cd
zanedeso <zanemilo@yahoo.com>
2025-05-19 13:59:22 -0400

    Update view engine setup to include app_server directory for views

--- travlr/app.js
+++ travlr/app.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var travelRouter = require('./routes/travel');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
