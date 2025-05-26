7ce29ed0c9d24e6d386163fc69b1ce318e1a1860
Zane Deso <zanemilo@yahoo.com>
2025-05-26 18:23:42 -0400

    Add note about JSON file reading practice in travel controller

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -2,6 +2,10 @@
 var fs = require('fs');
 var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
+// NOTE: It is not a best practice to read a JSON file every time the webserver
+// processes a request. This is a method used during development to support rapid
+// prototyping and should be replaced before the applications goes into production.
+
 
 /* GET travel view */
 const travel = (req, res) => {
