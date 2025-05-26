7d39dfd0fca60a5157e0c9f707e6046e7b049d81
Zane Deso <zanemilo@yahoo.com>
2025-05-26 18:22:46 -0400

    Load trips data from JSON file and pass to travel view for rendering

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,7 +1,11 @@
-/* GET Homepage */
 
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+
+/* GET travel view */
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
