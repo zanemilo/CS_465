8ba2fe3896aff454ae5e529cd54ec86325cb9714
zanedeso <zanemilo@yahoo.com>
2025-06-04 19:56:14 -0400

    Implement tripsList controller to retrieve all trips from the database

---
+++ travlr/app_api/controllers/trips.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); // Register model
+const Model = mongoose.model('trips');
+
+// GET: /trips - list all trips
+// Regardless of outcome, response must include HTML status code
+const tripsList = async (req, res) => {
+    const q = await Model
+    .find({}) //No filter returns all trips
+    .exec();
+
+    //Uncomment the following line to console log results of query
+    //console.log(q);
+
+    if(!q)
+    { // Database returned no data
+        return res.status(404).json({err});
+    } else {
+        return res.status(200).json(q);
+    }
+};
+
+module.exports = {
+    tripsList
+};
