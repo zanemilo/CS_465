7e8b19499eb48bec9dc8b168e55af4041ffe3c9a
zanedeso <zanemilo@yahoo.com>
2025-06-06 09:52:09 -0400

    Refactor travel controller to fetch trips from API and update view links to use trip codes

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,17 +1,36 @@
 
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+  method : 'GET',
+  headers: {
+    'Accept' : 'application/json',
+  }
+}
 
-// NOTE: It is not a best practice to read a JSON file every time the webserver
-// processes a request. This is a method used during development to support rapid
-// prototyping and should be replaced before the applications goes into production.
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 
-/* GET travel view */
-const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways', trips});
+const travel = async function (req, res, next) {
+  // console.log('TRAVEL CONTROLLER BEGIN');
+  await fetch(tripsEndpoint, options)
+    .then((res) => res.json())
+    .then((json) => {
+      let message = null;
+      if (!(json instanceof Array)) {
+        message = "API Lookup error";
+        json = [];
+      } else {
+        if (!json.length) {
+          message = "No trips exist in the database";
+        }
+      }
+      res.render("travel", {title: 'Travlr Getaways', trips: json, message});
+  })
+  .catch((err) => res.status(500).send(err.message));
 };
 
+
 module.exports = {
-  travel
+  travel,
 };
