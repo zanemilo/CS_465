5229eae161ed91c86bb1644aed8ba2f4531cc4ef
zanedeso <zanemilo@yahoo.com>
2025-06-06 09:20:31 -0400

    Add route for retrieving a single trip by code

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// Import the trips controllers to route
 const tripsController = require('../controllers/trips');
 
+// define route for our trips endpoint
 router.route('/trips').get(tripsController.tripsList)
+
+// GET: /trips/:tripCode - lists a single trip
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
 
 module.exports = router;
