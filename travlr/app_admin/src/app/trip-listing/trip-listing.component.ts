52ec53d53d2fe2d18015088fc9a899a4da098c0b
zanedeso <zanemilo@yahoo.com>
2025-06-18 11:40:05 -0400

    feat: Add TripListingComponent with template, styles, and test setup

---
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css'
+})
+export class TripListingComponent {
+
+}
