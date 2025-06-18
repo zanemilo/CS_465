b2a4c66616cb9a5a29ef07bffaa91bdde6253356
zanedeso <zanemilo@yahoo.com>
2025-06-18 14:37:12 -0400

    feat: Add trip interface to define trip model structure

---
+++ travlr/app_admin/src/app/models/trip.ts
@@ -0,0 +1,11 @@
+export interface trip {
+    _id: string, //Internal primary key in MongoDB
+    code: string,
+    name: string,
+    length: string,
+    start: Date,
+    resort: string,
+    perPerson: string,
+    image: string,
+    description: string,
+}
