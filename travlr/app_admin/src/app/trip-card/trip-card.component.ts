9794491d277ac197bbc4d3972cc118cf0976d58f
zanedeso <zanemilo@yahoo.com>
2025-06-18 12:15:58 -0400

    feat: Implement TripCardComponent with HTML template and CSS styles

---
+++ travlr/app_admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-card.component.html',
+  styleUrl: './trip-card.component.css'
+})
+export class TripCardComponent {
+
+}
