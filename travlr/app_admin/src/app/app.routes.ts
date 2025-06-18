22ceb363965822f5bbae91d9888413683f5cec92
zanedeso <zanemilo@yahoo.com>
2025-06-18 17:42:34 -0400

    feat: Define application routes for adding a trip and listing trips

--- travlr/app_admin/src/app/app.routes.ts
+++ travlr/app_admin/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { AddTripComponent } from './add-trip/add-trip.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'add-trip', component: AddTripComponent},
+    { path: '', component: TripListingComponent, pathMatch: 'full' }
+];
