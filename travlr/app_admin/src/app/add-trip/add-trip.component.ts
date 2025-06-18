b0540efa18015addfcb4a23a32db4b2ff876e28b
zanedeso <zanemilo@yahoo.com>
2025-06-18 17:55:24 -0400

    feat: Implement add trip form with validation and submission logic

--- travlr/app_admin/src/app/add-trip/add-trip.component.ts
+++ travlr/app_admin/src/app/add-trip/add-trip.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TripDataService } from '../services/trip-data.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-trip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-trip.component.html',
   styleUrl: './add-trip.component.css'
 })
-export class AddTripComponent {
+export class AddTripComponent implements OnInit {
+  addForm!: FormGroup;
+  submitted = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private tripService: TripDataService
+  ) { }
+
+  ngOnInit(): void {
+    this.addForm = this.formBuilder.group({
+      _id: [],
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  public onSubmit(): void {
+    this.submitted = true;
+
+    if (this.addForm.valid) {
+      this.tripService.addTrip(this.addForm.value)
+      .subscribe( {
+        next: (data: any) => {
+          console.log(data);
+          this.router.navigate(['']);
+        },
+        error: (error: any) => {
+          console.error('Error adding trip: ' + error);
+          console.log('Error details:', JSON.stringify(error, null, 2));
+        }
+      });
+    }
+  }
+  // get the form short name to access the form fields
+  get f() { return this.addForm.controls; }
 }
+
+
