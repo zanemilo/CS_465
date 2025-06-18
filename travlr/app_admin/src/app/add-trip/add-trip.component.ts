bed8453b3708fca5e1c48c68057c28aaf34816ca
zanedeso <zanemilo@yahoo.com>
2025-06-18 16:23:09 -0400

    feat: Implement TripListingComponent with trip loading functionality and add AddTripComponent files

---
+++ travlr/app_admin/src/app/add-trip/add-trip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-trip',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-trip.component.html',
+  styleUrl: './add-trip.component.css'
+})
+export class AddTripComponent {
+
+}
--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -3,20 +3,50 @@ import { CommonModule } from '@angular/common';
 import { trips } from '../data/trips';
 import { TripCardComponent } from '../trip-card/trip-card.component';
 
+import { Trip } from '../models/trip'; // Import the Trip interface if needed
+import { TripDataService } from '../services/trip-data.service'; // Import the service if you plan to use it
+
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
   imports: [CommonModule, TripCardComponent],
   templateUrl: './trip-listing.component.html',
-  styleUrl: './trip-listing.component.css'
+  styleUrl: './trip-listing.component.css',
+  providers: [TripDataService]
 })
-export class TripListingComponent implements OnInit {
-  trips = trips;
 
-  constructor() { }
+export class TripListingComponent implements OnInit {
+  trips!: Trip[]; // Declare trips as an array of Trip interface
+  message: string = 'Loading trips...'; // Message to display while loading
+
+  constructor(private tripDataService: TripDataService) { 
+    console.log('trip-listing constructor');
+  }
+
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+    .subscribe({
+      next: (value: any) => {
+        this.trips = value;
+        if(value.length > 0) 
+        { 
+          this.message = 'There are '+ value.length + ' trips available.';
+        }
+        else{
+          this.message = 'There were no trips retrieved from the database.';
+        }
+        console.log(this.message);
+      },
+      error: (error: any) => {
+        console.log('Error: ' + error);
+      }
+    })
+  }
+
 
   ngOnInit(): void {
-    // Any initialization logic can go here
+    console.log('trip-listing ngOnInit');
+    this.getStuff();
   }
 
 }
