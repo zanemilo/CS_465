1809dbd6f7bddfafdbf369fcf79bcca8ee6b4f6b
zanedeso <zanemilo@yahoo.com>
2025-06-18 11:42:29 -0400

    feat: Add trips data file with initial trip listings

---
+++ travlr/app_admin/src/app/data/trips.ts
@@ -0,0 +1,30 @@
+export const trips = [
+    {
+        "code": "GALR210214",
+        "name": "Gale Reef",
+        "length": "4 Nights / 5 days",
+        "start": "2021-02-14T08:00:00Z",
+        "resort": "Emerald Bay, 3 stars",
+        "perPerson": "799.00",
+        "image": "reef1.jpg",
+        "description": "Gale Reef Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis."
+    }, {
+        "code": "DAWR210315",
+        "name": "Dawson's Reef",
+        "length": "4 Nights / 5 days",
+        "start": "2021-03-15T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1199.00",
+        "image": "reef2.jpg",
+        "description": "Dawson's Reef Integer magna leo, posuere et dignissim vitae, porttitor at odio. Pellentesque a metus nec magna placerat volutpat. Nunc nisi mi, elementum sit amet aliquet quis, tristique quis nisl. Curabitur odio lacus, blandit ut hendrerit"
+    }, {
+        "code": "CLAR210621",
+        "name": "Claire's Reef",
+        "length": "4 Nights / 5 days",
+        "start": "2021-06-21T08:00:00Z",
+        "resort": "Coral Sands, 5 stars",
+        "perPerson": "1999.00",
+        "image": "reef3.jpg",
+        "description": "Claire's Reef vulputate, vulputate at est. Morbi aliquet viverra metus eu consectetur. In lorem dui, elementum sit amet convallis ac, tincidunt vel sapien."
+    }
+];
