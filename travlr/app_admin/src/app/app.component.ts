f45927ee0ceed8d7661390d6f7c80c6712dc59b3
zanedeso <zanemilo@yahoo.com>
2025-06-18 11:48:34 -0400

    feat: Integrate TripListingComponent into AppComponent and update template

--- travlr/app_admin/src/app/app.component.ts
+++ travlr/app_admin/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TripListingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
