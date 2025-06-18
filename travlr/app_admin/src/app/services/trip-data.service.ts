9de7115553810068a9e85da04f1e5abdcb8d8548
zanedeso <zanemilo@yahoo.com>
2025-06-18 17:58:14 -0400

    fix: Refactor URL handling in TripDataService for consistency

--- travlr/app_admin/src/app/services/trip-data.service.ts
+++ travlr/app_admin/src/app/services/trip-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { Trip } from '../models/trip'; // Ensure this import matches your model definition
 
 @Injectable({
@@ -11,10 +12,18 @@ import { Trip } from '../models/trip'; // Ensure this import matches your model 
 export class TripDataService {
 
   constructor(private http: HttpClient) { }
+  url = 'http://localhost:3000/api/trips';
 
   getTrips(): Observable<Trip[]> {
-    let url = 'http://localhost:3000/api/trips';
+    return this.http.get<Trip[]>(this.url);
+  }
 
-    return this.http.get<Trip[]>(url);
+  // This will satisfy the binding for the add-trip component 
+  // but it will not get us anywhere
+  // because we still need to add the HTML code to generate the form, 
+  // and we need to build the
+  // back-end API method to accept the posted form data.
+  addTrip(formData: Trip): Observable<Trip> {
+    return this.http.post<Trip>(this.url, formData);
   }
 }
