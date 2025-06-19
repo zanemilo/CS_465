90f415c10bb0d2b440b4eb9839d2c8999b13a012
zanedeso <zanemilo@yahoo.com>
2025-06-19 08:26:57 -0400

    feat: Implement EditTripComponent with form for editing trip details and associated service methods

---
+++ travlr/app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-trip',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-trip.component.html',
+  styleUrl: './edit-trip.component.css'
+})
+export class EditTripComponent {
+
+}
