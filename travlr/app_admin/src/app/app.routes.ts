ff0ef188fb2c5aeb7bc695319a75afb7091513bb
zanedeso <zanemilo@yahoo.com>
2025-06-19 08:44:45 -0400

    feat: Add route for EditTripComponent in app routing

--- travlr/app_admin/src/app/app.routes.ts
+++ travlr/app_admin/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
 import { AddTripComponent } from './add-trip/add-trip.component';
+import { EditTripComponent } from './edit-trip/edit-trip.component';
 
 export const routes: Routes = [
     { path: 'add-trip', component: AddTripComponent},
+    { path: 'edit-trip', component: EditTripComponent},
     { path: '', component: TripListingComponent, pathMatch: 'full' }
 ];
