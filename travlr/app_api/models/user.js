35c61ce45428b9b3c798ba93541bd4860e37173f
zanedeso <zanemilo@yahoo.com>
2025-06-26 10:55:16 -0600

    Add code for JWT in user.js model

--- travlr/app_api/models/user.js
+++ travlr/app_api/models/user.js
@@ -1,4 +1,6 @@
 const mongoose =  require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose .Schema({
     email : {
@@ -14,4 +16,42 @@ const userSchema = new mongoose .Schema({
         salt: String
 });
 
+// Method to set the password on this record.
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(
+        password,
+        this.salt,
+        1000,
+        64,
+        'sha512'
+    ).toString('hex');
+};
+
+// Method to compare entered password against stored hash
+userSchema.methods.validPassword = function(password) {
+    const hash = crypto.pbkdf2Sync(
+        password,
+        this.salt,
+        1000,
+        64,
+        'sha512'
+    ).toString('hex');
+    return this.hash === hash;
+};
+
+// Method to generate a JSON Web Token for the current record
+userSchema.methods.generateJWT = function() {
+    return jwt.sign(
+        { // Payload for our JSON Web Token
+            _id: this._id,
+            email: this.email,
+            name: this.name,
+        },
+        process.env.JWT_SECRET, // SECRET stored in .env file
+        { expiresIn: '1h' } // Token expires an hour from creation
+    );
+};
+
 mongoose.model('users', userSchema);
+
