cf74e3133f3eca965a810b44226a792a4a05dd39
zanedeso <zanemilo@yahoo.com>
2025-06-26 22:53:20 -0600

    feat: Implement LoginComponent with form handling and validation

--- travlr/app_admin/src/app/login/login.component.ts
+++ travlr/app_admin/src/app/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public formError: string = '';
+  submitted = false;
 
+  credentials = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+
+  ngOnInit(): void { }
 }
