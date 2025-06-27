84d5b7c12f1b9a4d863592bdf6f309c7aca3ddd3
zanedeso <zanemilo@yahoo.com>
2025-06-26 19:47:24 -0600

    feat: Add BROWSER_STORAGE injection token for localStorage

--- travlr/app_admin/src/app/storage.ts
+++ travlr/app_admin/src/app/storage.ts
@@ -1,2 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
+export const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
+  providedIn: 'root',
+  factory: () => localStorage
+});
+
 export class Storage {
 }
