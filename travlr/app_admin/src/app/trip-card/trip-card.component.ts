15a4beaf6503d96df1f14c2ca3707d8bbc62eb35
zanedeso <zanemilo@yahoo.com>
2025-06-27 08:33:41 -0600

    feat: Inject AuthenticationService into TripCardComponent constructor

--- travlr/app_admin/src/app/trip-card/trip-card.component.ts
+++ travlr/app_admin/src/app/trip-card/trip-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-card',
@@ -13,7 +14,10 @@ import { Trip } from '../models/trip';
 export class TripCardComponent implements OnInit {
   @Input('trip') trip: any;
 
-  constructor(private router : Router) { }
+  constructor(
+    private router : Router,
+    private authenticationService: AuthenticationService
+  ) { }
   ngOnInit(): void {
     // Initialization logic can go here if needed
   }
