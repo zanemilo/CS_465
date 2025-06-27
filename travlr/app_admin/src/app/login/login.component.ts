4c07b5f34ba69d965686fcd2c988b7eb5dc05bb1
zanedeso <zanemilo@yahoo.com>
2025-06-26 22:56:07 -0600

    feat: Add login functionality with form validation and user authentication

--- travlr/app_admin/src/app/login/login.component.ts
+++ travlr/app_admin/src/app/login/login.component.ts
@@ -28,4 +28,43 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void { }
+
+  // Called when the user clicks “Sign In!”
+public onLoginSubmit(): void {
+  this.formError = '';
+  if (!this.credentials.email ||
+      !this.credentials.password ||
+      !this.credentials.name) {
+    this.formError = 'All fields are required, please try again';
+    this.router.navigateByUrl('#'); // Return to login page
+  } else {
+    this.doLogin();
+  }
+}
+
+// Does the heavy lifting: builds a User, invokes the auth service, then redirects
+private doLogin(): void {
+  const newUser = {
+    name: this.credentials.name,
+    email: this.credentials.email
+  } as User;
+
+  // console.log('LoginComponent::doLogin');
+  // console.log(this.credentials);
+
+  this.authenticationService.login(newUser, this.credentials.password);
+
+  if (this.authenticationService.isLoggedIn()) {
+    // console.log('Router::Direct');
+    this.router.navigate(['']);
+  } else {
+    const timer = setTimeout(() => {
+      if (this.authenticationService.isLoggedIn()) {
+        // console.log('Router::Pause');
+        this.router.navigate(['']);
+      }
+    }, 3000);
+  }
+}
+
 }
