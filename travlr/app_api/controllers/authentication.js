5a2a0ecfa7f69779955479e852dff952e6669235
zanedeso <zanemilo@yahoo.com>
2025-06-26 19:20:00 -0600

    feat: Implement login endpoint with validation and JWT generation

--- travlr/app_api/controllers/authentication.js
+++ travlr/app_api/controllers/authentication.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const passport = require('passport');
 
 const register = async (req, res) => {
   // Validate message to ensure that all parameters are present
@@ -33,6 +34,39 @@ const register = async (req, res) => {
   }
 };
 
+const login = (req, res) => {
+    // Validate message to ensure that email and password are present.
+    if (!req.body.email || !req.body.password) {
+        return res
+            .status(400)
+            .json({"message": "All fields required"});
+    }
+
+    // Delegate authentication   to passport module
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            // Error in Authentication Process
+            return res
+                .status(404)
+                .json(err);
+        }
+
+        if (user) {  // Auth succeeded - generate JWT and return to caller
+            const token = user.generateJWT();
+            res
+                .status(200)
+                .json({token});
+        } else {  // Auth failed return error
+            res
+                .status(401)
+                .json(info);
+        }
+    })(req, res);
+};
+
+
+
 module.exports = {
-  register
+  register,
+  login
 };
