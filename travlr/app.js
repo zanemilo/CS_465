c6f4f6f5d85daa7439aa97f86c3199dd10214075
zanedeso <zanemilo@yahoo.com>
2025-06-26 19:35:29 -0600

    fix: Correctly import passport module and maintain configuration loading

--- travlr/app.js
+++ travlr/app.js
@@ -9,7 +9,8 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var apiRouter = require('./app_api/routes/index');
-var passport = require('./app_api/config/passport');
+const passport = require('passport');
+require('./app_api/config/passport'); 
 
 var handlebars = require('hbs');
 
